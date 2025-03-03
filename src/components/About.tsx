5ebf0eb6ff209c96cac325dd8abaa4cd65bb195e
sociocreatives <ayaraerick@gmail.com>
2025-03-03 22:31:21 +0300

    about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -8,12 +8,12 @@ export default function About() {
             <div className='max-w-7xl mx-auto '>
             <div className="flex flex-col-reverse md:grid md:grid-cols-2 gap-4">
                 <div className=''>
-                    <Image src="/images/about.png" alt="image" width={350} height={350} className='mt-10' />
+                    <Image src="/images/woman_shopping_image.png" alt="image" width={400} height={350} className='mt-8 md:-mt-15 px-5 md:px-0' />
                 </div>
                 <div className='flex flex-col justify-center items-center text-white'>
-                    <h1 className='text-4xl md:text-5xl font-bold text-center md:text-left  md:p-0 p-8 mb-4'>We tap into modern payment methods that help your business grow.</h1>
-                    <p>We help you accept global debit and credit card payments for all card brands and local payment methods.</p>
-                    <div className="flex flex-col items-start w-full mt-4">
+                    <h1 className='text-4xl md:text-5xl font-bold text-center md:text-left  md:px-0 px-8 mb-4 mt-16'>We tap into modern payment methods that help your business grow.</h1>
+                    <p className='p-6 text-center md:text-left md:p-0'>We help you accept global debit and credit card payments for all card brands and local payment methods.</p>
+                    <div className="flex flex-col items-center md:flex md:items-start w-full mt-4">
                     <Button className='bg-red-500 text-white border-red-500 hover:bg-red-800 hover:text-white px-6 py-5 rounded-full  sm:mx-0' >Get Started</Button>
                     </div>
                 </div>
