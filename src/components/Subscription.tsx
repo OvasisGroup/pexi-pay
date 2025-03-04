eca104bfb40e25908c8e0089edf9c93e41ab1465
sociocreatives <ayaraerick@gmail.com>
2025-03-04 04:40:56 +0300

    about

--- src/components/Subscription.tsx
+++ src/components/Subscription.tsx
@@ -9,7 +9,7 @@ export default function Subscription() {
             <div>
                 <Image src="/images/man-bags.png" alt="image" width={1000} height={100} className='md:-mt-20 mt-15 px-10 md:px-0' />
             </div>
-            <div className='flex flex-col justify-center items-start gap-4 px-16'>
+            <div className='flex flex-col justify-center items-start gap-4 md:px-16 px-6'>
                 <h1 className='text-white font-bold text-3xl'>Join Our Mailing List</h1>
                 <input type="email" placeholder="Enter your email address" className='w-full px-4 py-2 bg-white border border-gray-300 rounded-md focus:outline-none focus:border-maingreen' />
                 <button className='w-full px-4 py-2 border bg-mainsecondary border-mainsecondary rounded-md text-white font-bold hover:bg-red-700 focus:outline-none focus:border-mainsecodnary cursor-pointer'>Subscribe</button>
