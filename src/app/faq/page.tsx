3607a77924bd37a0e3fa25e9fe5c7d9af8406cfc
sociocreatives <ayaraerick@gmail.com>
2025-03-04 06:23:38 +0300

    about

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -3,20 +3,27 @@ import MainFooter from '@/components/MainFooter'
 import NavigationBar from '@/components/Navbar'
 import Subscription from '@/components/Subscription'
 import React from 'react'
+
 import {
     Accordion,
     AccordionContent,
     AccordionItem,
     AccordionTrigger,
   } from "@/components/ui/accordion"
-import Image from 'next/image'
+
+import { IoHomeOutline } from "react-icons/io5";
+import Link from 'next/link'
+
 
 export default function Faq() {
   return (
     <section>
         <NavigationBar/>
         <div className='relative w-full h-60 bg-[url("/images/faq.png")] bg-cover bg-center bg-no-repeat'>
-        
+        <div className='flex flex-col justify-center items-center h-full'>
+            <Link href={'/'}><p className='text-white'><span><IoHomeOutline size={20} className='text-white mb-4' /></span>/home/</p></Link>
+            <h1 className='font-bold text-white sm:text-3xl text-xl'>Frequently Asked Questions</h1>
+        </div>
         </div>
        
     <div className='max-w-7xl mx-auto py-6 my-16 px-6 md:px-0'>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState } from "react";
-import { Menu, X, ChevronDown, Settings, User } from "lucide-react";
+import { Menu, X, ChevronDown} from "lucide-react";
 import Link from "next/link";
 // import { useRouter } from "next/router";
 import Image from "next/image";
