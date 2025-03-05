f927d9557e818126ac48694a49d29481c5b27cca
sociocreatives <ayaraerick@gmail.com>
2025-03-05 20:13:20 +0300

    services

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -6,124 +6,102 @@ import React from 'react'
 import Image from "next/image";
 import About from '@/components/About'
 
-const services = [
+// const services = [
+//     {
+//         image: "/images/icon-pexi.png",
+//         name: "Core Payment Processing",
+//         description: [
+//             {
+//                 title: "Card Payments",
+//                 body: "Accept Visa, Mastercard, AMEX, and other credit/debit cards."
+//             },
+//             {
+//                 title: "Bank Transfers",
+//                 body: "Enable direct bank payments (ACH, SEPA, etc.)."
+//             },
+//             {
+//                 title: "Mobile Money Payments",
+//                 body: "Support M-Pesa, PayPal, Apple Pay, Google Pay, etc."
+//             },
+//             {
+//                 title: "Crypto Payments ",
+//                 body: "Allow payments in BTC, ETH, USDT, and other cryptocurrencies."
+//             },
+//             {
+//                 title: "Recurring Payments",
+//                 body: "Enable subscription billing with automated payments."
+//             },
+//             {
+//                 title: "QR Code Payments",
+//                 body: "Allow customers to pay by scanning a QR code."
+//             },
+//         ]
+//     },
+//     {
+//         image: "/images/icon-pexi.png",
+//         name: "Core Payment Processing",
+//         description: [
+//             {
+//                 title: "Card Payments",
+//                 body: "Accept Visa, Mastercard, AMEX, and other credit/debit cards."
+//             },
+//             {
+//                 title: "Bank Transfers",
+//                 body: "Enable direct bank payments (ACH, SEPA, etc.)."
+//             },
+//             {
+//                 title: "Mobile Money Payments",
+//                 body: "Support M-Pesa, PayPal, Apple Pay, Google Pay, etc."
+//             },
+//             {
+//                 title: "Crypto Payments ",
+//                 body: "Allow payments in BTC, ETH, USDT, and other cryptocurrencies."
+//             },
+//             {
+//                 title: "Recurring Payments",
+//                 body: "Enable subscription billing with automated payments."
+//             },
+//             {
+//                 title: "QR Code Payments",
+//                 body: "Allow customers to pay by scanning a QR code."
+//             },
+//         ]
+//     },
+// ]
+
+const teamMembers = [
     {
         image: "/images/icon-pexi.png",
-        name: "Core Payment Processing",
-        description: [
-            {
-                title: "Card Payments:",
-                body: "Accept Visa, Mastercard, AMEX, and other credit/debit cards."
-            },
-            {
-                title: "Bank Transfers:",
-                body: "Enable direct bank payments (ACH, SEPA, etc.)."
-            },
-            {
-                title: "Mobile Money Payments:",
-                body: "Support M-Pesa, PayPal, Apple Pay, Google Pay, etc."
-            },
-            {
-                title: "Recurring Payments:",
-                body: "Enable subscription billing with automated payments."
-            },
-            {
-                title: "QR Code Payments:",
-                body: "Allow customers to pay by scanning a QR code."
-            },
-        ]
+        name: "High-Risk Credit Card Processing",
+        description: "We are a payment gateway built and designed by a team with over seven years of industry experience. With this expertise, we have developed a system that incorporates the latest technology, including fraud detection, chargeback protection, and real-time transaction monitoring. This, in turn, ensures smooth onboarding and transparent pricing with no hidden costs.",
     },
     {
         image: "/images/icon-pexi.png",
-        name: "Security & Compliance",
-        description: [
-            {
-                title: "Fraud Detection & Prevention:",
-                body: "Implement AI-based fraud detection and KYC (Know Your Customer) verification."
-            },
-            {
-                title: "3D Secure Authentication (3DS2):",
-                body: "Enhance security for online card payments."
-            },
-            {
-                title: "PCI-DSS Compliance:",
-                body: "Ensure secure handling of cardholder data."
-            },
-            {
-                title: "AML & KYC Checks:",
-                body: "Prevent money laundering and ensure compliance with regulations."
-            },
-            {
-                title: "Dispute Management:",
-                body: "Allow chargeback handling and dispute resolution."
-            },
-        ]
+        name: "Alternative payment methods",
+        description: "Our robust Api incorporates different payment methods in Africa and the world, opening up more secure avenues, such as e wallets, vouchers, prepaid cards , direct debits and bank transfers and at the same time minimizing fraud and chargebacks",
     },
     {
         image: "/images/icon-pexi.png",
-        name: "Merchant & Customer Services",
-        description: [
-            {
-                title: "Merchant Dashboard:",
-                body: "A web interface for businesses to track transactions, manage payments, and generate invoices.."
-            },
-            {
-                title: "Customer Portal:",
-                body: "Let customers view transaction history and manage payment preferences."
-            },
-        ]
+        name: "Real-time reporting and analytics",
+        description: "View how you are performing, get access to real time information that allows you to pivot and make informed decisions on time and in time.",
     },
     {
         image: "/images/icon-pexi.png",
-        name: "API & Developer Tools",
-        description: [
-            {
-                title: "RESTful & GraphQL APIs: ",
-                body: "Allow seamless integration for merchants and developers."
-            },
-            {
-                title: "SDKs & Plugins:",
-                body: "Provide SDKs for JavaScript, Python, TypeScript, and plugins for Shopify, WooCommerce, Magento, etc."
-            },
-            {
-                title: "Webhooks & Notifications:",
-                body: "Notify merchants about payment statuses in real time."
-            },
-        ]
+        name: "Smart transaction routing",
+        description: "Analyse reports and make the informed decision to direct your transactions into the channels that offer best fees and returns, and ultimately improve approval ratings."
     },
     {
         image: "/images/icon-pexi.png",
-        name: "Reporting & Analytics",
-        description: [
-            {
-                title: "Transaction Reports: ",
-                body: "Generate daily, weekly, or monthly transaction summaries."
-            },
-            {
-                title: "Business Insights:",
-                body: "Provide analytics on sales trends, customer behavior, and payment performance."
-            },
-            {
-                title: "Automated Reconciliation:",
-                body: "Match transactions with bank settlements."
-            },
-        ]
+        name: "Fraud and charge back prevention ",
+        description: "Your business is safe with us, we continuously work with new age fraud and chargeback prevention methods, able to detect any hazardous threats in real time, and in return protecting you from any unforeseen losses."
     },
     {
         image: "/images/icon-pexi.png",
-        name: "Customer Support",
-        description: [
-            {
-                title: "Live Chat & Ticketing System: ",
-                body: "Provide 24/7 support via chat, email, and phone."
-            },
-            {
-                title: "Dispute Resolution Center:",
-                body: "Allow customers and merchants to resolve chargebacks and payment issues."
-            },
-        ]
+        name: "Payment processing for igaming",
+        description: "As a provider we take into consideration your verticals/risk industries and associated licenses. Keeping that in mind we have designated 24/7 customer support, instant escalated resolutions, multiple payment methods/multi currency support and a system designed to help you grow by offering a secure environment, and straight through  processing."
     },
-]
+
+];
 
 export default function Services() {
     return (
@@ -132,13 +110,13 @@ export default function Services() {
             <div className='relative w-full h-100 bg-[url("/images/faq.png")] bg-cover bg-center bg-no-repeat'>
                 <div className='flex flex-col justify-center items-center h-full'>
                     <h1 className='font-bold text-mainsecondary md:text-4xl text-3xl mb-6'>Our Services</h1>
-                    <h1 className='md:text-3xl text-2xl px-6 text-white md:max-w-200 text-center font-thin'>Building a payment processing gateway requires multiple services to ensure security, reliability, and compliance. </h1>
+                    <h1 className='md:text-3xl text-2xl px-6 text-white md:max-w-200 text-center font-thin'>We tap into modern payment methods that help your business grow. We help you accept global debit and credit card payments for all card brands and local payment methods.</h1>
                 </div>
             </div>
             <div className='grid md:grid-cols-2 gap-6 max-w-7xl mx-auto mt-15 mb-20'>
-                {services.map((member, index) => (
+                {teamMembers.map((member, index) => (
                     <div key={index} className="border-1 p-6 rounded-lg mx-6 md:mx-0 hover:bg-mainblue transition-colors hover:text-white">
-                        <div className='flex flex-col gap-6 justify-between items-start' >
+                        <div className='flex md:flex-row flex-col gap-6 justify-between items-start' >
                             <div>
                                 <Image
                                     src={member.image}
@@ -147,15 +125,11 @@ export default function Services() {
                                     height={80}
                                     className="mb-4"
                                 />
-                                <p className="text-xl font-semibold md:text-right text-left pr-4 text-mainsecondary mb-4">{member.name}</p>
+                                <p className="text-xl font-semibold text-left pr-4 text-mainsecondary mb-4">{member.name}</p>
+                                <p className="text-sm text-left mb-4">{member.description}</p>
                             </div>
                         </div>
-                        <p className='text-sm'>{member.description.map((item, index) => (
-                            <div key={index} className='flex flex-col md:gap-2 border-b-1 py-2'>
-                                <p className='text-sm font-semibold'>{item.title}</p>
-                                <p className='text-sm'>{item.body}</p>
-                            </div>
-                        ))}</p>
+                        
                     </div>
                 ))}
             </div>
