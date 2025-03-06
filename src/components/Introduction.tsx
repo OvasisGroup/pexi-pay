baebf17d95e39866eb3881997bf857854ea2845a
sociocreatives <ayaraerick@gmail.com>
2025-03-06 13:48:12 +0300

    Removed Open Banking

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -8,6 +8,7 @@ import { MdOutlineAnalytics } from "react-icons/md";
 import { AiOutlineTransaction } from "react-icons/ai";
 import { GoShieldCheck } from "react-icons/go";
 import { VscServerProcess } from "react-icons/vsc";
+import Link from "next/link";
 
 
 const teamMembers = [
@@ -26,11 +27,11 @@ const teamMembers = [
         image: <MdOutlinePayments size={60} className='text-mainsecondary' />,
         description: "Blockchain Dev, Full-time Degen with hands-on experience in decentralized finance and cryptocurrency.",
     },
-    {
-        name: "Open Banking",
-        image: <BsBank size={60} className='text-mainsecondary' />,
-        description: "Blockchain Dev, Full-time Degen with hands-on experience in decentralized finance and cryptocurrency.",
-    },
+    // {
+    //     name: "Open Banking",
+    //     image: <BsBank size={60} className='text-mainsecondary' />,
+    //     description: "Blockchain Dev, Full-time Degen with hands-on experience in decentralized finance and cryptocurrency.",
+    // },
     {
         name: "Real – time reporting & Analytics",
         image: <MdOutlineAnalytics size={60} className='text-mainsecondary' />,
@@ -64,13 +65,18 @@ export default function Introduction() {
                     <h1 className='font-bold text-mainblue text-4xl my-1 px-6 md:px-0'>We deliver outstanding results while streamlining your processes.</h1>
                     <p className='px-6 md:px-0'>We deliver outstanding results while streamlining your processes. Our goal is to maximize efficiency and ensure you achieve the best outcomes with minimal effort and expense.</p>
                 </div>
-                <div className='grid grid-cols-2 md:grid-cols-4 gap-4 md:gap-4 p-6 md:p-0'>
+                <div className='grid grid-cols-1 md:grid-cols-4 gap-4 md:gap-4 p-6 md:p-0'>
                 {teamMembers.map((member, index) => (
-                        <div key={index} className="bg-maingrey  rounded-lg hover:rounded-4xl transition-all hover:bg-mainblue transition-colors hover:text-white flex flex-col justify-center items-center min-h-60 p-10 text-center">
+                        <div key={index} className="bg-maingrey  rounded-lg hover:rounded-4xl transition-all hover:bg-mainblue transition-colors hover:text-white flex md:flex-col flex-row gap-4 md:gap-0 md:justify-center items-center md:min-h-60 p-10 md:text-center">
                             <div>{member.image}</div>
-                            <p className="  mt-4">{member.name}</p>
+                            <p className="mt-4">{member.name}</p>
                         </div>
                     ))}
+                    <div className="bg-mainblue rounded-lg hover:rounded-4xl transition-all hover:text-white flex flex-col justify-end items-center min-h-60 p-10 text-center hidden md:block">
+                        <Link href="/signin" className='w-full'>
+                            <p className='bg-mainsecondary w-full text-white py-4 px-10 rounded-xl font-bold hover:bg-red-700 cursor-pointer hidden md:block'>Get Started &rarr;</p>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
